5985d37f64b548d084bb4f5f02c8f7b35ae72d34
douglas <douglas.campos@mojix.com>
2026-03-01 10:33:06 -0400

    remove rewrite

--- cv-ai-manager/next.config.mjs
+++ cv-ai-manager/next.config.mjs
@@ -2,14 +2,14 @@
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
-  async rewrites() {
-    return [
-      {
-        source: '/api/backend/:path*',
-        destination: `${process.env.AWS_EC2_URL}/:path*`,
-      },
-    ]
-  },
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: '/api/backend/:path*',
+  //       destination: `${process.env.AWS_EC2_URL}/:path*`,
+  //     },
+  //   ]
+  // },
 };
 
 export default nextConfig;
